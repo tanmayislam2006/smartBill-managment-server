ff0df3de979d50657f0cdee0984b690ab7d937de
Your Name <you@example.com>
2025-05-17 10:26:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+app.use(cors());
+app.use(express.json());
+// console.log(process.env.DB_USER, process.env.DB_PASSWORD);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kn8r7rw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+
+    await client.connect();
+    const smartBillUsersCollection=client.db("smartBillUser").collection("smartUsers")
+    const createdBillCollection=client.db("createdBillCollection").collection("createdBill")
+    app.post("/register",async (req,res)=>{
+      const userInformation =req.body
+      const result=await smartBillUsersCollection.insertOne(userInformation)
+      res.send(result)
+    })
+    // get all bill
+    app.get("/mybill/:uid",async(req,res)=>{
+      const uid=req.params.uid
+      const query={uid:uid}
+      const result=await createdBillCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.post("/createdbill",async(req,res)=>{
+      const billInformation =req.body
+      const result=await createdBillCollection.insertOne(billInformation)
+      res.send(result)
+    })
+    // deleted specific bil 
+    app.delete('/bill/:id',async(req,res)=>{
+      const id =req.params.id
+      const query={_id:new ObjectId(id)}
+      const result=await createdBillCollection.deleteOne(query)
+      res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("server work succesfully");
+});
+
+app.listen(port, () => {
+  console.log(`server is runnig on http://localhost:${port}`);
+});
